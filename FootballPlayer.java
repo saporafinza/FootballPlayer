465e80d8d6b0f97000b716e2fe098267e5d1dc6d
saporafinza <98521049+saporafinza@users.noreply.github.com>
2022-08-03 16:51:36 +0300

    Main files and codes add.

---
+++ FootballPlayer.java
@@ -0,0 +1,26 @@
+
+public class FootballPlayer {
+  
+  String Ronaldo;
+  String name;
+  int no;
+  boolean inPlay;
+  int minutes;
+  int numberOfGoals;
+  
+  void play(int minutesToPlay) {
+    minutes = minutes + minutesToPlay;
+    
+  }
+  
+  void score() {
+    numberOfGoals = numberOfGoals + 1;
+    
+  }
+  
+  
+  
+  
+  
+      
+}
---
+++ FootballPlayerTest.java
@@ -0,0 +1,32 @@
+
+public class FootballPlayerTest {
+
+	public static void main(String[] args) {
+
+		FootballPlayer Ronaldo = new FootballPlayer();
+		
+		Ronaldo.name = "Cristiano Ronaldo";
+		Ronaldo.no = 7;
+		Ronaldo.inPlay = true;
+		Ronaldo.minutes = 15;
+		Ronaldo.numberOfGoals = 1;
+		
+		
+		Ronaldo.play(0);
+		Ronaldo.score();
+		Ronaldo.score();
+		
+		System.out.println("Number of goals Ronaldo scored: " + Ronaldo.numberOfGoals);
+		System.out.println("In minutes to play Ronaldo " + Ronaldo.minutes);
+		System.out.println("Ronaldo have " + Ronaldo.no + " form number" );
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
